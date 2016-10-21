46c9f0d01a0f7ca94713d9b6cf8b8ba664b95a65
Leo Keuken <leonard.keuken@sky.uk>
2016-10-21 10:12:10 +0100

    script repo setup for cleaning go-server artifacts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # go-server-cleanup
+
+run the script from the machine where your go-server is installed.
+
+It assumes having the default setup and file structure:
+'/var/lib/go-server/artifacts/pipelines'
+
+By default it will chuck anything older than 180 days. Optionally on the bash command you can pass a number as argument to set a deviating amount of days you wish to retain.
+
+By default it will always keep at least 15 of your last builds.
+Also nothing will get deleted in a pipeline if there is 15 builds or less.
---
+++ cleanup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+datediff() {
+    d1=$(date -d "$today" +%s)
+    d2=$(date -d "$fileChanged" +%s)
+    echo $(( (d1 - d2) / 86400 ))
+}
+
+today=$(date -d 'now')
+daysInput=$1
+
+if [ -z $daysInput ];then
+  daysInput=180
+  echo "set default 180 days retention"
+fi
+
+basedir="/var/lib/go-server/artifacts/pipelines"
+cd "${basedir}"
+
+pipelines=$(ls -la | awk '{if($9 > 0) print $9}')
+
+for pipeline in $pipelines;
+do
+  cd "${pipeline}"
+
+  buildsCount=$(ls -la | awk '{if( $9 > 0 ) print $9}' | grep -c $)
+  echo ${buildsCount} " builds in " $(pwd)
+
+  allBuilds=$(ls -lart | awk '{if($9 > "0") print $9}')
+  #only start if there is more than 15 builds in the pipeline
+  if [ $buildsCount -gt 15 ]; then
+    for directory in $allBuilds;
+    do
+      #evaluate build count in each iteration
+      if [ $buildsCount -gt 15 ]; then
+
+        fileChanged=$(stat -c %z $directory)
+        days=$(datediff)
+
+        if [ ${days} -gt ${daysInput} ]; then
+          rm -r $directory
+          echo $directory "is deleted, it was "$days " days old"
+         #update build count after deleting 1
+         buildsCount=$(( $buildsCount - 1 ))
+       fi
+     else
+       echo "15 builds or less left in this pipeline, not deleting anymore"
+     fi
+   done
+  else
+   echo "15 builds or less in pipeline, not deleting anything"
+  fi
+ cd ..
+done
